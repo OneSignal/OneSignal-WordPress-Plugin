8b0688223ad4eb523d1e899715f91cc1bf2848b1
Jason Pang <jason@onesignal.com>
2016-02-22 15:48:37 -0800

    Bump version.

--- onesignal.php
+++ onesignal.php
@@ -3,7 +3,7 @@
   * Plugin Name: OneSignal Push Notifications
  * Plugin URI: https://onesignal.com/
  * Description:
- * Version: 1.9.0
+ * Version: 1.9.1
  * Author: OneSignal
  * Author URI: https://onesignal.com
  * License: MIT

203f5a2b1485d8c42dfdba9a7540b1589b1d29c9
Jason Pang <jason@jasonpang.me>
2016-06-02 18:22:04 -0700

    Bump version, increase developer count

--- onesignal.php
+++ onesignal.php
@@ -3,7 +3,7 @@
   * Plugin Name: OneSignal Push Notifications
  * Plugin URI: https://onesignal.com/
  * Description: Free web push notifications.
- * Version: 1.10.6
+ * Version: 1.11.0
  * Author: OneSignal
  * Author URI: https://onesignal.com
  * License: MIT

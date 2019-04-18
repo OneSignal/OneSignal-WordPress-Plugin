41222241aa9108ce86d6de4c8ed972f4d9c6e587
Rodrigo <rogomezpalacio@protonmail.com>
2019-04-18 15:02:52 -0700

    Added response body as error message to debug tool when the recipient count is zero

--- onesignal-admin.php
+++ onesignal-admin.php
@@ -824,6 +824,9 @@ public static function uuid($title) {
 		$status = $response['response']['code'];
 	}
 
+    if ($response_body['recipients'] == "0") {
+        update_post_meta($post->ID, "error_message", json_encode($response_body));
+    }
 
 	update_post_meta($post->ID, "status", $status);
 	

ff366f5e02b0c9142079ae94f5d6cdf18f46f493
Rodrigo <rogomezpalacio@protonmail.com>
2019-05-22 16:46:18 -0700

    1.17.4 release commit

--- onesignal.php
+++ onesignal.php
@@ -6,7 +6,7 @@ defined( 'ABSPATH' ) or die('This page may not be accessed directly.');
  * Plugin Name: OneSignal Push Notifications
  * Plugin URI: https://onesignal.com/
  * Description: Free web push notifications.
- * Version: 1.17.3
+ * Version: 1.17.4
  * Author: OneSignal
  * Author URI: https://onesignal.com
  * License: MIT

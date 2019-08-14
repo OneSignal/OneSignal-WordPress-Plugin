081ec933320c9eee3fb219651e6033dbb3b7348b
Rodrigo Gomez Palacio <rodrigo@onesignal.com>
2019-08-13 18:13:30 -0700

    1.17.9 Release Commit (#208)
    * 1.17.9 Release Commit
    - Added 30 second delay to scheduled notification send time
    - Bug fix: added post time as input to idempotency hash function 
       - Reason: previously didn't cancel first scheduled notification when the second was scheduled since it thought it was the same

--- onesignal.php
+++ onesignal.php
@@ -1,36 +1,34 @@
 <?php
 
-defined( 'ABSPATH' ) or die('This page may not be accessed directly.');
+defined('ABSPATH') or die('This page may not be accessed directly.');
 
-/**
+/*
  * Plugin Name: OneSignal Push Notifications
  * Plugin URI: https://onesignal.com/
  * Description: Free web push notifications.
- * Version: 1.17.8
+ * Version: 1.17.9
  * Author: OneSignal
  * Author URI: https://onesignal.com
  * License: MIT
  */
 
-define( 'ONESIGNAL_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define('ONESIGNAL_PLUGIN_URL', plugin_dir_url(__FILE__));
 
-/**
+/*
  * The number of seconds required to wait between requests.
  */
-define( 'ONESIGNAL_API_RATE_LIMIT_SECONDS', 1 );
-define( 'ONESIGNAL_URI_REVEAL_PROJECT_NUMBER', 'reveal_project_number=true' );
+define('ONESIGNAL_API_RATE_LIMIT_SECONDS', 1);
+define('ONESIGNAL_URI_REVEAL_PROJECT_NUMBER', 'reveal_project_number=true');
 
-require_once( plugin_dir_path( __FILE__ ) . 'onesignal-utils.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'onesignal-admin.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'onesignal-public.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'onesignal-settings.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'onesignal-widget.php' );
+require_once plugin_dir_path(__FILE__).'onesignal-utils.php';
+require_once plugin_dir_path(__FILE__).'onesignal-admin.php';
+require_once plugin_dir_path(__FILE__).'onesignal-public.php';
+require_once plugin_dir_path(__FILE__).'onesignal-settings.php';
+require_once plugin_dir_path(__FILE__).'onesignal-widget.php';
 
-if (file_exists(plugin_dir_path( __FILE__ ) . 'onesignal-extra.php')) {
-    require_once( plugin_dir_path( __FILE__ ) . 'onesignal-extra.php' );
+if (file_exists(plugin_dir_path(__FILE__).'onesignal-extra.php')) {
+    require_once plugin_dir_path(__FILE__).'onesignal-extra.php';
 }
 
-add_action( 'init', array( 'OneSignal_Admin', 'init' ) );
-add_action( 'init', array( 'OneSignal_Public', 'init' ) );
-
-?>
+add_action('init', array('OneSignal_Admin', 'init'));
+add_action('init', array('OneSignal_Public', 'init'));

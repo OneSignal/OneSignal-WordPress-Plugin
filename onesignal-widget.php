8e47c7a37b6c44e0b6812307e854af70db19e1e9
Davis Shaver <davisshaver@gmail.com>
2019-11-30 09:47:28 -0500

    217: Cleanup spacing in widget file

--- onesignal-widget.php
+++ onesignal-widget.php
@@ -3,30 +3,29 @@
 defined( 'ABSPATH' ) or die('This page may not be accessed directly.');
 
 class OneSignalWidget extends WP_Widget {
-  
+
 	function __construct() {
-    parent::__construct('OneSignalWidget', 'OneSignal', array( 'description' => 'Subscribe to notifications'));
+		parent::__construct('OneSignalWidget', 'OneSignal', array( 'description' => 'Subscribe to notifications'));
 	}
-  
-  // Admin editor
+
+	// Admin editor
 	function form($instance) {
-    $title = ! empty( $instance['title'] ) ? $instance['title'] : 'Follow';
-    $text = ! empty( $instance['text'] ) ? $instance['text'] : 'Subscribe to notifications';
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : 'Follow';
+		$text = ! empty( $instance['text'] ) ? $instance['text'] : 'Subscribe to notifications';
 		?>
 		<p>
 		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
 		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
-    		<label for="<?php echo $this->get_field_id( 'text' ); ?>"><?php _e( 'Body:' ); ?></label> 
+		<label for="<?php echo $this->get_field_id( 'text' ); ?>"><?php _e( 'Body:' ); ?></label> 
 		<input class="widefat" id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>" type="text" value="<?php echo esc_attr( $text ); ?>">
 		</p>
 		<?php 
 	}
-  
+
 	function update($new_instance, $old_instance) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-    $instance['text'] = ( ! empty( $new_instance['text'] ) ) ? strip_tags( $new_instance['text'] ) : '';
-    
+		$instance['text'] = ( ! empty( $new_instance['text'] ) ) ? strip_tags( $new_instance['text'] ) : '';
 		return $instance;
 	}
 

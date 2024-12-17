f13f2ca01f748d0dd304a5817cf3f711d146ec59
Rodrigo Gomez Palacio <rodrigo@onesignal.com>
2024-12-17 15:53:42 -0600

    Update v3 Plugin Initialization to use plugin-defined SW settings
    Motivation: to ensure a smooth transition, we will handle SW installation in the plugin. We need to update v3 to do that

--- v3/onesignal-init.php
+++ v3/onesignal-init.php
@@ -7,15 +7,20 @@ add_action('wp_head', 'onesignal_init');
 
 function onesignal_init()
 {
+  $onesignal_wp_settings = get_option('OneSignalWPSetting');
 ?>
   <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
   <script>
-    window.OneSignalDeferred = window.OneSignalDeferred || [];
-    OneSignalDeferred.push(async function(OneSignal) {
-      await OneSignal.init({
-        appId: "<?php echo get_option('OneSignalWPSetting')['app_id']; ?>",
-      });
-    });
-  </script>
+          window.OneSignalDeferred = window.OneSignalDeferred || [];
+          OneSignalDeferred.push(async function(OneSignal) {
+            await OneSignal.init({
+              appId: "<?php echo esc_html($onesignal_wp_settings['app_id']); ?>",
+              serviceWorkerOverrideForTypical: true,
+              path: "<?php echo ONESIGNAL_PLUGIN_URL; ?>sdk_files/",
+              serviceWorkerParam: { scope: '/' },
+              serviceWorkerPath: 'OneSignalSDKWorker.js.php',
+            });
+          });
+        </script>
 <?php
 }

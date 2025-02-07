8edb7850232ac610d63e93926a03419b25f46f32
sherwinski <sherwin@onesignal.com>
2025-02-07 13:26:41 -0800

    test: load in bespoke custom post type for testing
    Source: https://developer.wordpress.org/plugins/post-types/registering-custom-post-types/

--- v3/onesignal-init.php
+++ v3/onesignal-init.php
@@ -5,6 +5,13 @@ defined('ABSPATH') or die('This page may not be accessed directly.');
 // Add OneSignal initialisation code to head of pages.
 add_action('wp_head', 'onesignal_init');
 
+// Add any development-only features as long as user is an admin
+add_action('init', function() {
+    if (is_admin() && test_plugin_is_development()) {
+        test_plugin_register_post_types();
+    }
+});
+
 function onesignal_init()
 {
   $onesignal_wp_settings = get_option('OneSignalWPSetting');
@@ -46,4 +53,45 @@ function onesignal_init()
           });
         </script>
 <?php
+}
+
+/**
+ * Register custom post types for testing
+ * Only loads in development/testing environments
+ */
+
+ function test_plugin_register_post_types() {
+  register_post_type('test_product',
+    array(
+      'labels'            => array(
+          'name'          => __('Test Products', 'test-plugin'),
+          'singular_name' => __('Test Product', 'test-plugin'),
+      ),
+      'public'       => true,
+      'has_archive'  => true,
+      'rewrite'      => array('slug' => 'test-products'),
+      'supports'     => array('title', 'editor', 'thumbnail', 'excerpt'),
+      'show_in_rest' => true,
+    )
+  );
+}
+
+/**
+ * Check if we're in a development environment
+ *
+ * @return boolean
+ */
+function test_plugin_is_development() {
+  return (
+      // Check for common development environment constants
+      defined('WP_DEBUG') && WP_DEBUG
+      // ||
+      // defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ||
+      // // Check if we're in the test environment
+      // defined('WP_TESTS_DOMAIN') ||
+      // // Check if we're in a local environment
+      // strpos($_SERVER['HTTP_HOST'] ?? '', 'localhost') !== false ||
+      // strpos($_SERVER['HTTP_HOST'] ?? '', '.test') !== false ||
+      // strpos($_SERVER['HTTP_HOST'] ?? '', '.local') !== false
+  );
 }
